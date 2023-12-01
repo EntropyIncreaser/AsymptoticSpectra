f0e27aeb9ca1f48d841aea1d019d2f667df1bd9d
Elegia Inferno <eirig@qq.com>
2023-12-02 01:06:32 +0800

    subadditive

--- AsymptoticSpectra.lean
+++ AsymptoticSpectra.lean
@@ -7,8 +7,8 @@ noncomputable section
 open Classical
 
 class PreorderCommSemiring (α : Type u) extends CommSemiring α, Preorder α where
-  add_le_add_left : ∀ a b : α, a ≤ b → ∀ c : α, a + c ≤ b + c
-  mul_le_mul_left : ∀ a b : α, a ≤ b → ∀ c : α, a * c ≤ b * c
+  add_le_add_left : ∀ a b : α, a ≤ b → ∀ c : α, c + a ≤ c + b
+  mul_le_mul_left : ∀ a b : α, a ≤ b → ∀ c : α, c * a ≤ c * b
   zero_le_one : (0 : α) ≤ 1
 
 class StrassenPreorderCommSemiring (α : Type u) extends PreorderCommSemiring α where
@@ -18,18 +18,36 @@ class StrassenPreorderCommSemiring (α : Type u) extends PreorderCommSemiring α
 
 variable [StrassenPreorderCommSemiring R]
 
-lemma StrassenPreorderCommSemiring.add_le_add (a b c d : R) : a ≤ b → c ≤ d → a + c ≤ b + d := sorry
+lemma StrassenPreorderCommSemiring.add_le_add (a b c d : R) : (h₁ : a ≤ b) → (h₂ : c ≤ d) → a + c ≤ b + d := sorry
+
+lemma StrassenPreorderCommSemiring.mul_le_mul (a b c d : R) : (h₁ : a ≤ b) → (h₂ : c ≤ d) → a * c ≤ b * d := sorry
+
+namespace StrassenPreorderCommSemiring
 
 def rank (a : R) : ℕ := Nat.find (StrassenPreorderCommSemiring.upper_archimedean a)
 
 theorem rank.monotone (a b : R) : a ≤ b → rank a ≤ rank b := by {
   intro h
   let n := rank b
-  have h' : b ≤ n := Nat.find_spec (StrassenPreorderCommSemiring.upper_archimedean b)
+  have h' : b ≤ n := Nat.find_spec (upper_archimedean b)
   apply Nat.find_min'
   exact le_trans h h'
 }
 
-theorem rank.subadditive (a b : R) : rank (a + b) ≤ rank a + rank b := sorry
+theorem rank.subadditive (a b : R) : rank (a + b) ≤ rank a + rank b := by {
+  let n := rank a
+  let m := rank b
+  have h₁ : a ≤ n := Nat.find_spec (upper_archimedean a)
+  have h₂ : b ≤ m := Nat.find_spec (upper_archimedean b)
+  apply Nat.find_min'
+  simp
+  exact add_le_add _ _ _ _ h₁ h₂
+}
 
-theorem rank.submultiplicative (a b : R) : rank (a * b) ≤ rank a * rank b := sorry
+theorem rank.submultiplicative (a b : R) : rank (a * b) ≤ rank a * rank b := by {
+  let n := rank a
+  let m := rank b
+  have h₁ : a ≤ n := Nat.find_spec (upper_archimedean a)
+  have h₂ : b ≤ m := Nat.find_spec (upper_archimedean b)
+  sorry
+}
