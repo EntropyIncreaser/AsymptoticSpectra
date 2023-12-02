2d9ca4661c69da968cdd02092d3d7945115c3ebc
Elegia Inferno <eirig@qq.com>
2023-12-02 11:02:04 +0800

    Use Order Instead

--- AsymptoticSpectra.lean
+++ AsymptoticSpectra.lean
@@ -1,30 +1,30 @@
-import Mathlib.Algebra.Ring.Defs
-import Mathlib.Init.Order.Defs
-import Mathlib.Init.Data.Nat.Lemmas
+import Mathlib.Data.Nat.Cast.Order
 
 noncomputable section
 
 open Classical
 
-class PreorderCommSemiring (α : Type u) extends CommSemiring α, Preorder α where
-  add_le_add_left : ∀ a b : α, a ≤ b → ∀ c : α, c + a ≤ c + b
-  mul_le_mul_left : ∀ a b : α, a ≤ b → ∀ c : α, c * a ≤ c * b
-  zero_le_one : (0 : α) ≤ 1
-
-class StrassenPreorderCommSemiring (α : Type u) extends PreorderCommSemiring α where
-  embed_nat : ∀ n m : ℕ, n ≤ m ↔ (n : α) ≤ m
+class StrassenOrderedCommSemiring (α : Type u) extends OrderedCommSemiring α, CharZero α where
   lower_archimedean : ∀ a : α, a = 0 ∨ 1 ≤ a
   upper_archimedean : ∀ a : α, ∃ n : ℕ, a ≤ n
 
-variable [StrassenPreorderCommSemiring R]
+variable [StrassenOrderedCommSemiring R]
 
-lemma StrassenPreorderCommSemiring.add_le_add (a b c d : R) : (h₁ : a ≤ b) → (h₂ : c ≤ d) → a + c ≤ b + d := sorry
+example (a b c d : R) : a ≤ b → c ≤ d → a + c ≤ b + d := add_le_add
+example {m n : ℕ} : (m : R) ≤ n ↔ m ≤ n := Nat.cast_le
+example : ℕ ↪o R := Nat.castOrderEmbedding
 
-lemma StrassenPreorderCommSemiring.mul_le_mul (a b c d : R) : (h₁ : a ≤ b) → (h₂ : c ≤ d) → a * c ≤ b * d := sorry
+-- lemma StrassenOrderedCommSemiring.mul_le_mul (a b c d : R) : (h₁ : a ≤ b) → (h₂ : c ≤ d) → a * c ≤ b * d := sorry
 
-namespace StrassenPreorderCommSemiring
+namespace StrassenOrderedCommSemiring
 
-def rank (a : R) : ℕ := Nat.find (StrassenPreorderCommSemiring.upper_archimedean a)
+theorem all_nonneg (a : R) : 0 ≤ a := by {
+  cases (lower_archimedean a)
+  case inl h => { rw [h] }
+  case inr h => { exact le_trans zero_le_one h }
+}
+
+def rank (a : R) : ℕ := Nat.find (StrassenOrderedCommSemiring.upper_archimedean a)
 
 theorem rank.monotone (a b : R) : a ≤ b → rank a ≤ rank b := by {
   intro h
@@ -41,7 +41,7 @@ theorem rank.subadditive (a b : R) : rank (a + b) ≤ rank a + rank b := by {
   have h₂ : b ≤ m := Nat.find_spec (upper_archimedean b)
   apply Nat.find_min'
   simp
-  exact add_le_add _ _ _ _ h₁ h₂
+  exact add_le_add h₁ h₂
 }
 
 theorem rank.submultiplicative (a b : R) : rank (a * b) ≤ rank a * rank b := by {
@@ -49,5 +49,7 @@ theorem rank.submultiplicative (a b : R) : rank (a * b) ≤ rank a * rank b := b
   let m := rank b
   have h₁ : a ≤ n := Nat.find_spec (upper_archimedean a)
   have h₂ : b ≤ m := Nat.find_spec (upper_archimedean b)
-  sorry
+  apply Nat.find_min'
+  simp
+  exact mul_le_mul h₁ h₂ (all_nonneg b) (all_nonneg ↑n)
 }
