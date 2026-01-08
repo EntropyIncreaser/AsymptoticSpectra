c4cba4f15d68b5f47bd68c886dc3fbbbf36bb0c7
Elegia Inferno <eirig@qq.com>
2026-01-08 13:26:48 -0500

    docs

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,9 @@ package «asymptoticSpectra» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
+
 @[default_target]
 lean_lib «AsymptoticSpectra» {
   -- add any library configuration options here
